6d1a909d71087ed2470590e1e3cb5334c6a662da
Enzo Avalos <enzoavalos97@gmail.com>
2022-03-02 07:39:17 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
+
 
 import { AppComponent } from './app.component';
+import { PaisesModule } from './paises/paises.module';
+import { SharedModule } from './shared/shared.module';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    PaisesModule,
+    SharedModule,
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/paises/pages/ver-pais/ver-pais.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { switchMap, tap } from 'rxjs/operators';
+
+import { Country } from '../../interfaces/paises.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-ver-pais',
+  templateUrl: './ver-pais.component.html',
+  styles: [],
+})
+export class VerPaisComponent implements OnInit {
+  //puede ser null
+  pais!: Country[];
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisService
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ idPais }) => this.paisService.getPaisPorCodigo(idPais)),
+        tap(console.log)
+      )
+      .subscribe((pais) => {
+       this.pais = pais;
+      });
+
+    // this.activatedRoute.params.subscribe(({ idPais }) => {
+    //   this.paisService.getPaisPorCodigo(idPais).subscribe((pais) => {
+    //     console.log(pais);
+    //   });
+    // });
+  }
+
+
+}
